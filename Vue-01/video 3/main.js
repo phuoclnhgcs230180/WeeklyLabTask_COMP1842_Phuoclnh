99bf2162dc9f4d1b3460e060d657a5356691ebe0
phuoclnhgcs230180 <phuoclnhgcs230180@fpt.edu.vn>
2026-04-03 09:14:42 +0700

    Vue-01

---
+++ Vue-01/video 3/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image:`./assets/images/socks_green.jpg`,
+            url:'https://translate.google.com/?sl=vi&tl=en&op=translate'
+        }
+        }
+    })
+
---
+++ Vue-01/video 4/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image:`./assets/images/socks_green.jpg`,
+            inventory: 0,
+            onSale: true
+        }
+        }
+    })
+
